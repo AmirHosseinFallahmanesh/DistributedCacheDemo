c8f68d6d76a787f6ded466347329eb850015fc15
Administrator <Administrator@DESKTOP-DDFGGPH>
2024-12-22 23:07:49 -0800

    Add project files.

---
+++ Demo1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Demo1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IDistributedCache distributedCache;
+
+        public HomeController(ILogger<HomeController> logger,IDistributedCache distributedCache)
+        {
+            _logger = logger;
+            this.distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("key", Guid.NewGuid().ToString());
+           // distributedCache.SetString("data", Guid.NewGuid().ToString());
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            string data = HttpContext.Session.GetString("key");
+            return Content(data);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
